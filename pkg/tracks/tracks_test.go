4b35c7d53ed672089738d431ffad89911ad729e8
Lucas Ponce <lponce@redhat.com>
2025-05-24 17:37:12 +0200

    v0.0.2

--- pkg/tracks/tracks_test.go
+++ pkg/tracks/tracks_test.go
@@ -12,8 +12,8 @@ func TestTracks(t *testing.T) {
 
 	version := GetVersion()
 
-	if version != "v0.0.1" {
-		t.Errorf("Version %s, want %s", version, "v0.0.1")
+	if version != "v0.0.2" {
+		t.Errorf("Version %s, want %s", version, "v0.0.2")
 	}
 
 	tracks := GetTracks()
