ee696fac849c55af7c1d56ac26b904c4aa6408b3
Lucas Ponce <lponce@redhat.com>
2025-05-24 12:23:02 +0200

    tests

--- pkg/tracks/tracks.go
+++ pkg/tracks/tracks.go
@@ -13,11 +13,18 @@ func RemoveTrack(id string) {
 	delete(TRACKS, id)
 }
 
+func GetVersion() string {
+	return VERSION
+}
+
+func GetTracks() map[string]string {
+	return TRACKS
+}
+
 func DumpTracks() {
 	fmt.Printf("VERSION: %s TRACKS:\n", VERSION)
 	fmt.Println("-----------------------")
 	for k, v := range TRACKS {
 		fmt.Printf("%s: %s\n", k, v)
 	}
-
 }
---
+++ pkg/tracks/tracks_test.go
@@ -0,0 +1,24 @@
+package tracks
+
+import "testing"
+
+func TestTracks(t *testing.T) {
+
+	AddTrack("k1", "v1")
+	AddTrack("k2", "v2")
+	AddTrack("k3", "v3")
+
+	RemoveTrack("k3")
+
+	version := GetVersion()
+
+	if version != "v0.0" {
+		t.Errorf("Version %s, want %s", version, "v0.0")
+	}
+
+	tracks := GetTracks()
+	if len(tracks) != 2 {
+		t.Errorf("GetTracks returned %d tracks, want 2", len(tracks))
+	}
+	
+}
